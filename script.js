e040345518c724617e7548b92af2aced007f3249
Hannah Bush <heb.rutgers@gmail.com>
2022-01-30 19:37:33 -0500

    some problems, some solutions. working on it

--- script.js
+++ script.js
@@ -6,8 +6,9 @@
 //add points if correct
 //deduct time if wrong
 //save score if high score in localStorage
-var qCounter = 0;
-var submitAnwerr = document.querySelector("#save-task");
+var qCounter = 1;
+var score = 0;
+var submitAnswer = document.querySelector("#save-task");
 var quizQuestions = [
   {
     question: "Are you ready to start the quiz?",
@@ -16,7 +17,6 @@ var quizQuestions = [
     correctAnswer: "yes"
   },
   {
-    questionId: 1,
     question: "What are the two scopes of variables in Javascript?",
     answerA: "Abroad and Local",
     answerB: "Global and Local",
@@ -25,7 +25,6 @@ var quizQuestions = [
     correctAnswer: "answerB"
   },
   {
-    questionId: 1,
     question: "How do we access a value stored in an object?",
     answerA: "Dot notation, Curl bracket notation",
     answerB: "Period notation, Square bracket notation",
@@ -34,7 +33,6 @@ var quizQuestions = [
     correctAnswer: "answerD"
   },
   {
-    questionId: 2,
     question: "From the reason listed below which is NOT true about JavaScript?",
     answerA: "JavaScript increases interactivity of our websites.",
     answerB: "JavaScript lets provide the user immediate feedback upon an action.",
@@ -43,7 +41,6 @@ var quizQuestions = [
     correctAnswer: "answerC"
   },
   {
-    questionId: 3,
     question: "Inside the HTML document, where do you place your JavaScript code?",
     answerA: "In the <footer> element",
     answerB: "Inside the <link> element",
@@ -52,7 +49,6 @@ var quizQuestions = [
     correctAnswer: "answerD"
   },
   {
-    questionId: 4,
     question: "What are the six primitive data types in JavaScript?",
     answerA: "string, number, boolean, bigInt, symbol, undefined",
     answerB: "string, num, falsy, bigInt, symbol, undefined",
@@ -63,15 +59,17 @@ var quizQuestions = [
 ];
 
 var startQuestion = function() {
-  //set h2 eleemt to start question
+  //set h2 element to start question
   var h2Label = document.querySelector('#questionH2');
   h2Label.textContent = (quizQuestions[qCounter].question);
 
   //set radio options for yes and no
   var label = document.querySelector('#labelA');
   label.textContent = (quizQuestions[qCounter].answer1);
+  document.getElementById("answerA").value = (quizQuestions[qCounter].answer1);
   label = document.querySelector('#labelB');
   label.textContent = (quizQuestions[qCounter].answer2);
+  document.getElementById("answerB").value = (quizQuestions[qCounter].answer2);
 
   //hide unused radio elements and labels
   document.getElementById("C").style.display = "none";
@@ -86,12 +84,16 @@ var quizQuestion = function() {
   //set radio button text here
   var label = document.querySelector('#labelA');
   label.textContent = (quizQuestions[qCounter].answerA);
+  document.getElementById("answerA").value = (quizQuestions[qCounter].answerA);
   label = document.querySelector('#labelB');
   label.textContent = (quizQuestions[qCounter].answerB);
+  document.getElementById("answerB").value = (quizQuestions[qCounter].answerB);
   label = document.querySelector('#labelC');
   label.textContent = (quizQuestions[qCounter].answerC);
+  document.getElementById("answerC").value = (quizQuestions[qCounter].answerC);
   label = document.querySelector('#labelD');
   label.textContent = (quizQuestions[qCounter].answerD);
+  document.getElementById("answerD").value = (quizQuestions[qCounter].answerD);
 }
 
 var displayQuestion = function () {
@@ -104,12 +106,33 @@ var displayQuestion = function () {
   }
 };
 
+//check if answer is correct
+var checkAnswer = function () {
+  if (document.querySelector('input[name="radioGroup"]:checked').value === quizQuestions[qCounter].correctAnswer){
+    //add points to score
+    score+=20;
+    console.log(score);
+  } //else {
+    //if incorrect, deduct time from timer
+  //}
+}
+
+var endQuiz = function () {
+  //hide quiz question div
+  document.getElementById("quizForm").style.display = "none";
+  //display end screen with score
+  var divEl = document.createElement("div");
+  divEl.className = "endScreen";
+  divEl.innerHTML =
+  "<h2 class='endScreen'>You've finished the Quiz!</h2><br /><h3 class='endScreen'>Your score is " + score + "!</h3>";
+}
+
 var answerQuestion = function () {
   //load question
   displayQuestion();
   if (qCounter === 0){
     //check if user is ready for the quiz
-    if (document.getElementById('answerA').checked){
+    if (document.getElementById("answerA").checked){
       window.alert("You are not ready, select 'yes' when ready!");
       return false;
     }
@@ -117,18 +140,20 @@ var answerQuestion = function () {
     //timer code HERE
     //increment question counter
     qCounter++;
-  } else {
+    console.log(qCounter);
+  } else if (qCounter > 0 && qCounter < quizQuestions.length) {
     //load question
     displayQuestion();
     //event listener for submit answer button
-    submitAnwerr.addEventListener("submit", checkAnswer);
-    //check if answer is correct
-    var checkAnswer = function () {
-      if (){
-        
-      }
-    }
+    submitAnswer.addEventListener("submit", checkAnswer);
+    //increment question counter
+    qCounter++;
+    console.log(qCounter);
+  } else {
+    qCounter = 0;
+    endQuiz();
   }
+  answerQuestion();
 };
 
 answerQuestion();
